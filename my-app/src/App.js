40f7866b85dd52a219639e50d1489f2e02a1a692
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-13 09:32:48 +0000

    finsished test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import css from "./App.module.css";
+import NavBarForm from "./components/NavBarForm";
 import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarSimple />
+      <NavBarForm />
+      {/* <NavBarSimple /> */}
       <Sidebar />
     </div>
   );
